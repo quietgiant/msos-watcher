3e3f75709422902ecff839d96b5fafcb9b0c568e
Kurtis Taylor <kjtaylor1995@gmail.com>
2022-07-04 20:23:21 -0400

    fix: exit -> return

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handler(event, context):
 def main():
     if is_holiday(get_now_est()):
         print("Holiday detected, exiting...")
-        exit(0)
+        return
     update_holdings()
     diff = calculate_deltas()
     _ = post_message_to_slack(diff)
